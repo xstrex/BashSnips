46c784fd8a1b843d24e766673b2caea88377de0b
strex <strex@morphx.net>
2018-03-26 22:09:15 -0600

    minor code edits

--- vim_setup.sh
+++ vim_setup.sh
@@ -4,15 +4,10 @@
 #
 # Created by Strex @ 3/26/18
 #
-# mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-vim=$(which vim)
-sudo=$(which sudo)
-curl=$(which curl)
-
-if [ -n "$vim" ]; then
+if [ -n "$(vim)" ]; then
 	printf "Great, Vim's installed, proceeding\\n"
-elif [[ -z $vim ]]; then
+elif [ -z "$(vim)" ]; then
 	printf "Looks like Vim's not installed\\n"
 	printf "Trying to install Vim\\n"
 	vim-install
@@ -23,16 +18,14 @@ if [ "$(uname)" == "Darwin" ]; then
         OS="Darwin"
 elif [ "$(uname)" == "Linux" ]; then
         OS="Linux"
-elif [ "$(uname)" == "MINGW32_NT" ]; then
-        OS="Cygwin"
-elif [ "$(uname)" == "MINGW64_NT" ]; then
+elif [ "$(uname)" == "MINGW64_NT" ] || [ "$(uname)" == "MINGW64_NT" ]; then
         OS="Cygwin"
 fi
 
 # Vim Install function
 vim-install () {
-	if [ -n "$sudo" ]; then
-		if [[ -z $($sudo -n true) ]]; then
+	if [ -n "$(sudo)" ]; then
+		if [[ -z $(sudo -n true) ]]; then
 			printf "Looks like we have sudo access..\\n"
 			printf "Installing vim..\\n"
 			if [ $OS == "Darwin" ]; then
@@ -57,28 +50,28 @@ vim-install () {
 
 # Make vim directories
 printf "Setting up directories\\n"
-eval mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/syntax ~/.vim/colors ~/.vim/ftdetect
+mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/syntax ~/.vim/colors ~/.vim/ftdetect
 
 # Installing custom vimrc file
 printf "Setting up custom vimrc file\\n"
-eval "$curl" -LSso ~/.vim/vimrc https://raw.githubusercontent.com/xstrex/BashSnips/master/vim.rc
+curl -LSso ~/.vim/vimrc https://raw.githubusercontent.com/xstrex/BashSnips/master/vim.rc
 
 # Setting up pathogen
 printf "Setting up pathogen.vim\\n"
-eval "$curl" -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Installing Solarized Theme
 printf "Installing distinguished theme\\n"
-eval "$curl" -LSso ~/.vim/colors/distinguished.vim https://raw.githubusercontent.com/xstrex/BashSnips/master/Vim-Colors/distinguished.vim
+curl -LSso ~/.vim/colors/distinguished.vim https://raw.githubusercontent.com/xstrex/BashSnips/master/Vim-Colors/distinguished.vim
 
 # Installing Vividchalk Theme
 printf "Installing Vividchalk theme\\n"
-eval "$curl" -LSso ~/.vim/colors/vividchalk.vim https://raw.githubusercontent.com/xstrex/BashSnips/master/Vim-Colors/vividchalk.vim
+curl -LSso ~/.vim/colors/vividchalk.vim https://raw.githubusercontent.com/xstrex/BashSnips/master/Vim-Colors/vividchalk.vim
 
 # Installing monit syntax highlighting
 printf "Installing Monit syntax highlighting\\n"
-eval "$curl" -LSso ~/.vim/syntax/monitrc.vim https://raw.githubusercontent.com/xstrex/BashSnips/master/Vim-Syntax/monitrc.vim
-eval "$curl" -LSso ~/.vim/ftdetect/monitrc.vim https://raw.githubusercontent.com/xstrex/BashSnips/master/Vim-Syntax/monitrc.ft
+curl -LSso ~/.vim/syntax/monitrc.vim https://raw.githubusercontent.com/xstrex/BashSnips/master/Vim-Syntax/monitrc.vim
+curl -LSso ~/.vim/ftdetect/monitrc.vim https://raw.githubusercontent.com/xstrex/BashSnips/master/Vim-Syntax/monitrc.ft
 
 # Done
 printf "Vim is installed and configured\\n"
