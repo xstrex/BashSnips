19380f48002343d45305194d226e3dd0f5c37b87
strex <strex@morphx.net>
2018-03-29 19:42:46 -0600

    initial commit

---
+++ gpx-post.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+#
+# Generate a Dokuwiki post from a gpx file
+# @author Strex
+#
+# xpath 12312017.gpx '/gpx/metadata/time' | gsed -re 's/<\/?\w+>//g'
+#
+# Functions
+############
+usage () {
+	echo "Usage: $0 [ gpx files ]"
+}
+
+if [[ $# == '0' ]]; then
+	usage
+	return 0
+fi
